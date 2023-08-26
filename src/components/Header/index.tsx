7ed471b4584d102f9d17b7acc8147bfd43aabe04
neopromic <cusaowesley1@gmail.com>
2023-08-26 07:28:10 -0300

    feat: first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="sticky bg-white flex items-center gap-2 px-4 inset-x-0 top-0 bg-opacity-60 backdrop-blur-lg backdrop-filter py-4 z-50">
+      <img
+        src="/assets/logo.png"
+        className="w-6 h-6 rounded-full"
+        alt="ForDevs"
+      />
+      <p>ForDevs</p>
+    </header>
+  );
+}
